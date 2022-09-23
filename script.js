8e09123f5c75ab33cda1c7ef25e49ca95bfcec9c
JuzerNejm10 <107032742+JuzerNejm10@users.noreply.github.com>
2022-09-23 22:26:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas  = document.getElementById('canvas1');
+const canvas  = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
